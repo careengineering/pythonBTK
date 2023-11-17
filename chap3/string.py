9cf3555da77aac8fea6d6e8fd1dfb506c471e535
John DoeCesur <cesurkedix@gmail.com>
2023-11-17 16:54:23 +0300

    first commit

---
+++ chap3/string.py
@@ -0,0 +1,9 @@
+name="aaaa"
+surname="bbbb"
+age=111
+greeting="my name is "+ name + " " + surname + " and I am " +str(age) +" years old."
+print(greeting)
+print(greeting[-1])
+print(len(greeting))
+# print(greeting[length-1])
+print(greeting[2:5])
